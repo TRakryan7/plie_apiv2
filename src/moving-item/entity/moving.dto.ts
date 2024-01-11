d261ba27ce97f51b8e2e2b96760022d95c6ce8e2
TRakryan7 <Trakryan7@gmail.com>
2024-01-11 10:05:39 +0700

    fixing moving items

--- src/moving-item/entity/moving.dto.ts
+++ src/moving-item/entity/moving.dto.ts
@@ -6,6 +6,21 @@ export class MovingHeaderDto {
   warehouseId: string;
   createdBy: string;
   updatedBy: string;
+  detail: [
+    {
+      itemDescription: string;
+      size: string;
+      category: string;
+      color: string;
+      total: number;
+      shelvesId: string;
+      itemId: string;
+      rowsId: string;
+      period: number;
+      createdBy: string;
+      updatedBy: string;
+    },
+  ];
 }
 
 export class MovingDetailDto {
